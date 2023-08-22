2332da2031fc4a3cbe85ce61bacb30480d5a5a78
sonyafenge <45471193+sonyafenge@users.noreply.github.com>
2023-08-22 15:17:09 -0700

    Add teams pages to display teams groups/channels and files

--- samples/app/react-contoso/src/pages/TeamsPage.tsx
+++ samples/app/react-contoso/src/pages/TeamsPage.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import { 
-  FileList,
-  Get
-} from '@microsoft/mgt-react';
-import { makeStyles, Spinner } from '@fluentui/react-components';
+import { ResponseType } from '@microsoft/microsoft-graph-client';
+import { MgtTeamsChannelPicker, FileList } from '@microsoft/mgt-react';
+import { makeStyles} from '@fluentui/react-components';
 import { Tree, TreeItem, TreeItemLayout } from '@fluentui/react-tree';
-import { Loading } from '../components/Loading';
+import { ChevronRightRegular } from '@fluentui/react-icons';
+import { Team, Channel } from '@microsoft/microsoft-graph-types';
+import { IGraph, prepScopes, Providers } from '@microsoft/mgt-element';
 import { PageHeader } from '../components/PageHeader';
-
+import { Loading } from '../components/Loading';
 
 const useStyles = makeStyles({
   container: {
@@ -22,116 +22,180 @@ const useStyles = makeStyles({
     width: '30%'
   },
 
+  divider: {
+    width: '1px',
+    backgroundColor: '#D9D9D9'
+  },
+
   channelFiles: {
     display: 'flex',
     flexDirection: 'column',
     flexWrap: 'nowrap',
-    width: '70%',
+    width: '69%',
     '--file-list-box-shadow': 'none'
-  },
-
-  loading: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '4vh'
-  },
-
-  loadingMessage: {
-    paddingLeft: '10px'
-  },
+  }
 });
 
-const MyTeams = (props) => {
-  const teams = props.dataContext.value;
-  const { getSelectedTeamId } = props;
-  console.log(props);
+const ChannelsTree = (props) => {
+  const [channels, setChannels] = React.useState<Channel[]>([]);
+  const [loading , setLoading] = React.useState(false);
+  const [selectedChannelId, setSelectedChannelId] = React.useState('');
+  const { 
+    teamId, 
+    graph, 
+    selectedTeamId, 
+    setSelectedTeamId, 
+    setSelectedChannelName 
+  } = props;
+  const styles = useStyles();
 
-  const onTeamClick = (id) => {
-    getSelectedTeamId(id);
+
+  const getChannels = async () => {
+    setLoading(true);
+
+    try {
+      const curChannels = await getChannelsByTeam(graph, teamId, MgtTeamsChannelPicker.requiredScopes)
+      setChannels(curChannels);
+    } catch(error) {
+      console.error(error);
+      getChannels();
+    } finally {
+      setLoading(false);
+    }
   }
 
-  return(
-    <Tree>
-      {teams && (
-        teams.map((team) => (
-          <TreeItem itemType='branch' onClick={() => onTeamClick(team.id)}>
-            <TreeItemLayout>{team.displayName}</TreeItemLayout>
-            <Tree>
-              <Get
-                resource={`teams/${team.id}/channels`}
-              >
-                <MyChannels template='value'
-                  getSelectedChannelName={props.getSelectedChannelName}
-                ></MyChannels>
-                <ChannelsLoading template='loading'></ChannelsLoading>
-              </Get>
-            </Tree>
-          </TreeItem>
-        ))
-      )}
+  const getSelectedTeamChannel = (channelName, channelId) => {
+    setSelectedTeamId(teamId);
+    setSelectedChannelName(channelName);
+    setSelectedChannelId(channelId)
+  }
+
+  React.useEffect(() => {
+    getChannels();
+  }, []);
+
+  return (
+    <Tree aria-label='ChannelTree'>
+      {channels.map((channel) => (
+        <TreeItem 
+          itemType='leaf' 
+          key={channel.id}
+          onClick={() => {
+            getSelectedTeamChannel(channel.displayName, channel.id)
+          }}
+        >
+          <TreeItemLayout 
+            aside={(selectedChannelId === channel.id && selectedTeamId === teamId) ? 
+              <ChevronRightRegular /> : null}
+          >
+            <div style={{marginLeft:'1.2vh'}}>
+              {channel.displayName}
+            </div>
+          </TreeItemLayout>
+        </TreeItem>
+      ))}
     </Tree>
   );
 }
 
-const ChannelsLoading = (props) => {
-  const styles = useStyles();
+const TeamImg = (props) => {
+  const { graph, teamId } = props;
+  const [teamPhoto, setTeamPhoto] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
 
-  return(
-    <div className={styles.loading}>
-      <Spinner size='tiny'/>
-      <div className={styles.loadingMessage}>
-        <span>{props.message || 'Loading...'}</span>
-      </div>
-    </div>
-  );
-}
+  const getCurPhoto = async () => {
+    setLoading(true);
 
-const MyChannels = (props) => {
-  const { getSelectedChannelName } = props;
-  const { displayName } = props.dataContext;
-
-  const onChannelClick = () => {
-    getSelectedChannelName(displayName);
+    try {
+      const photo = await getTeamPhoto(graph, teamId, MgtTeamsChannelPicker.requiredScopes) as string;
+      setTeamPhoto(photo);
+    } catch (error) {
+      console.error(error);
+      getCurPhoto();
+    } finally {
+      setLoading(false);
+    }
   }
 
-  return(
-    <TreeItem itemType='leaf' onClick={onChannelClick}>
-      <TreeItemLayout style={{ marginLeft:'3vh' }}>{displayName}</TreeItemLayout>
-    </TreeItem>
+  React.useEffect(() => {
+    getCurPhoto();
+  }, []);
+
+  return (
+    <img 
+      src={teamPhoto} 
+      alt='' 
+      style={{ width: '25px', borderRadius: '4px', marginRight:'1vh' }}
+    ></img>
   );
 }
 
+//main component
 export const ChannelFilesPage: React.FunctionComponent = () => {
+  const provider = Providers.globalProvider;
+  const graph = provider.graph;
   const styles = useStyles();
+  const [loading, setLoading] = React.useState(false);
+  const [teams, setTeams] = React.useState<Team[]>([]);
   const [selectedTeamId, setSelectedTeamId] = React.useState('');
   const [selectedChannelName, setSelectedChannelName] = React.useState('');
+  
 
-  const getSelectedTeamId = (teamId) => {
-    setSelectedTeamId(teamId);
-  }
-  const getSelectedChannelName = (channelName) => {
-    setSelectedChannelName(channelName);
+  const getTeams = async () => {
+    setLoading(true);
+
+    try {
+      const teams = await getAllMyTeams(graph, MgtTeamsChannelPicker.requiredScopes);
+      setTeams(teams);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+
   }
 
+  React.useEffect(() => {
+    getTeams();
+  }, []);
+  
   return (
     <>
       <PageHeader
-        title='Group Channel Files'
+        title='Channel Files'
         description='View files from access channels you are a member of'
       ></PageHeader>
       <div className={styles.container}>
-        <Get
-          resource='/me/joinedTeams'
-          className={styles.teamChannel}
-        >
-          <MyTeams template='default'
-            getSelectedTeamId={getSelectedTeamId}
-            getSelectedChannelName={getSelectedChannelName}
-          ></MyTeams>
-          <Loading template='loading'></Loading>
-        </Get>
+        <div className={styles.teamChannel}>
+          {loading ? <Loading /> : 
+            <Tree aria-label='TeamTree'>
+              {teams.map((team) => (
+                <TreeItem itemType='branch' key={team.id}>
+                  <TreeItemLayout>
+                    <div style={{ display: 'flex' }}>
+                      <span style={{ display: 'flex', alignItems: 'center' }}>
+                        <TeamImg teamId={team.id} graph={graph} />
+                      </span>
+                      <span style={{ display: 'flex', alignItems: 'center' }}>
+                        {team.displayName}
+                      </span>
+                    </div>
+                  </TreeItemLayout>
+                  <ChannelsTree 
+                    teamId={team.id} 
+                    graph={graph} 
+                    selectedTeamId={selectedTeamId}
+                    setSelectedTeamId={setSelectedTeamId}
+                    setSelectedChannelName={setSelectedChannelName}
+                  />
+                </TreeItem>
+              ))}
+            </Tree>
+          }
+        </div>
 
+        <div className={styles.divider}></div>  
+        
         { selectedChannelName !== '' ? (
           <FileList
             groupId={selectedTeamId}
@@ -145,4 +209,48 @@ export const ChannelFilesPage: React.FunctionComponent = () => {
       </div>
     </>
   );
+}
+
+//utils
+
+const blobToBase64 = (blob: Blob) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result as string);
+    };
+    reader.readAsDataURL(blob);
+  });
 };
+
+const getAllMyTeams = async (graph: IGraph, scopes: string[]) => {
+  const teams = await graph
+    .api('/me/joinedTeams')
+    .select(['displayName', 'id'])
+    .middlewareOptions(prepScopes(...scopes))
+    .get();
+
+  return teams?.value || [];
+};
+
+const getTeamPhoto = async (graph: IGraph, teamId: string, scopes: string[]) => {
+  const response = (await graph
+    .api(`/teams/${teamId}/photo/$value`)
+    .responseType(ResponseType.RAW)
+    .middlewareOptions(prepScopes(...scopes))
+    .get()
+  ) as Response;
+
+  const blob = await blobToBase64(await response.blob());
+  return blob;
+};
+
+const getChannelsByTeam = async (graph: IGraph, teamId, scopes: string[]) => {
+  const channels = await graph
+    .api(`/teams/${teamId}/channels`)
+    .middlewareOptions(prepScopes(...scopes))
+    .get();
+
+  return channels?.value || [];
+}
